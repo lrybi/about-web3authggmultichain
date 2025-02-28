1c3be467e822ec02953bbb1b9cbeccc59cce4029
thinh <llllpppptttt2003@gmail.com>
2025-02-28 07:27:45 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,11 @@
 
 import { useEffect, useState } from "react";
 import { Web3AuthNoModal } from "@web3auth/no-modal";
-import { AuthAdapter, AuthAdapterOptions } from "@web3auth/auth-adapter";
-import { CHAIN_NAMESPACES, WEB3AUTH_NETWORK, UX_MODE, Web3AuthNoModalOptions, IProvider, WALLET_ADAPTERS } from "@web3auth/base";
+import { AuthAdapter } from "@web3auth/auth-adapter";
+import { CHAIN_NAMESPACES, IProvider, WALLET_ADAPTERS } from "@web3auth/base";
 import "../App.css";
 import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
-
-//import { web3AuthConfig, authAdapterConfig } from "../config/web3auth";
-const clientId = "BCx5UY9LtuNMxzW1IwGo8xW2o6wzDGkVDYmN725HJ01TPM8tXkjKM2ahBIatINZHtsT4iFTquCOOMMcwAsBuNRA"; // get from https://dashboard.web3auth.io
-const chainConfig = {
-    chainNamespace: CHAIN_NAMESPACES.EIP155,
-    chainId: "0xaa36a7",
-    rpcTarget: "https://rpc.ankr.com/eth_sepolia",
-    displayName: "Sepolia Testnet",
-    blockExplorerUrl: "https://sepolia.etherscan.io/",
-    ticker: "ETH",
-    tickerName: "Ethereum Sepolia",
-    logo: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
-  };
-  
+import { web3AuthConfig, authAdapterConfig } from "../config/web3auth";
 
 import type { NextPage } from "next";
 
@@ -42,22 +29,10 @@ const App: NextPage = () => {
   useEffect(() => {
     const init = async () => {
       try {
-        const privateKeyProvider = new EthereumPrivateKeyProvider({ config: { chainConfig } });
-        const web3AuthConfig: Web3AuthNoModalOptions = {
-          clientId,
-          privateKeyProvider,
-          web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
-        };
         const web3auth = new Web3AuthNoModal(web3AuthConfig);
         setWeb3auth(web3auth);
 
-        const authAdapterConfig: AuthAdapterOptions ={
-          adapterSettings: {
-            uxMode: UX_MODE.REDIRECT,
-          },
-        };
         const authAdapter = new AuthAdapter(authAdapterConfig);
-
         web3auth.configureAdapter(authAdapter);
 
         await web3auth.init();
